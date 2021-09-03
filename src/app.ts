ea2602dc02f8c424971facc266f2fcd16412bce6
Eduardo Peláez <eapelaezc@correo.url.edu.gt>
2021-09-02 22:49:40 -0600

    cambio parámetros

--- src/app.ts
+++ src/app.ts
@@ -7,16 +7,18 @@ app.use(express.json());
 
 let options = {
   pythonOptions: ['-u'],
-  args: ['-a value1', '-b value2']
+  args: ['2020-08-05 05:10:25-07', '2021-08-05 05:12:25-07', '2']
 };
 
 app.get('/', (req, res) => {
- PythonShell.run('D:/Josue/Documents/Landivar/DECIMO SEMESTRE/PROYECTOS/PruebasPY/src/example.py', options,
+  console.log('prueba api');
+  
+ PythonShell.run('/home/azureuser/virtualenv/infra-data/copiaRobot2.py', options,
     function (err, result) {
       if (err)
-        throw res.send(err);
-      console.log('resultado: %j', result);
-      res.send('sucess');
+        return res.status(500).json({ err });
+      
+      return res.status(200).json({ result });            
     }
   )
 });
